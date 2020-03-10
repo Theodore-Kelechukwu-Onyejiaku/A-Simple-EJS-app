d261a369eaf82ea663ececce9509f4a1042562e8
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-03-10 15:03:20 -0700

    add views

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
+//a simple rendering of an EJS template engine
 app.get("/", (req, res)=>{
-    res.end("Hello World")
+    res.render("index.ejs")
+})
+
+//Passing values to template engine
+app.get("/user/:id/:username", (req, res)=>{
+    const username = req.params.username;
+    const id = req.params.id;
+    res.render("user.ejs", {
+        username : username,
+        id: id
+    });
 })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
-    console.log("Server running succesfully")
+    console.log("Server running succesfully on port:"+PORT)
 })
